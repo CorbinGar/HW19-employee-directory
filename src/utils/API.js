522765b30a60e46730e2d2e3bb6d93387479b65d
Corbin Garner <Corbingar@gmail.com>
2021-06-05 09:50:01 -0700

    auditing

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+
 const employeeAmount = Math.floor(Math.random() * 20 + 9)
 const URL = `https://randomuser.me/api/?results=${employeeAmount}`
+
 export default {
     getEmps: function () {
         return axios.get(URL);
