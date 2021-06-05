04a0c03568903055806cb59168dab011586463bc
Corbin Garner <Corbingar@gmail.com>
2021-06-05 06:57:10 -0700

    small api changes

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-
-const URL = `https://randomuser.me/api/?results=20`
-
+const employeeAmount = Math.floor(Math.random() * 20 + 9)
+const URL = `https://randomuser.me/api/?results=${employeeAmount}`
 export default {
-        getEmps: function () {
-                return axios.get(URL);
-        }
+    getEmps: function () {
+        return axios.get(URL);
+    }
 }
