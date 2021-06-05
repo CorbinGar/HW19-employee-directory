8a44fc65f47b01e266cba592be2f8e35d7fa690f
Corbin Garner <Corbingar@gmail.com>
2021-06-05 03:23:39 -0700

    filled out navbar and started employee.js

---
+++ src/components/Employee/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import "./style.css";
+import Search from "../Search";
+import Table from "../Table";
+import API from "../../utils/API";
+import moment from "moment";
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="main-header">
+
+            <h1>Employee Diectory</h1>
+
+        </nav>
+    )
+}
+
+export default Navbar;
